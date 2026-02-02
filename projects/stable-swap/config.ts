1c5f2aeb3b124235d43155f0aa8fe59c4feb239c
MUROKO <muroko@users.noreply.github.com>
2026-02-02 06:09:47 -0500

    update_24_7: bsc/config

--- projects/stable-swap/config.ts
+++ projects/stable-swap/config.ts
@@ -59,7 +59,7 @@ export default {
 	//For - Tics
     ticsMainnet: "0xCFC6A070404c068f21Bc675B03F7c45331b7Fbe1", //official
 	//For - Bsc
-    bscMainnet: "0x", //official
+    bscMainnet: "0xCFC6A070404c068f21Bc675B03F7c45331b7Fbe1", //official
 	//For - Avax
     avaxMainnet: "0x", //official
   },
@@ -71,7 +71,7 @@ export default {
 	//For - Tics
     ticsMainnet: "0x6BFC02F34d09846dfE2555eAEB61fe7c5dd700F6", //official
 	//For - Bsc
-    bscMainnet: "0x", //official
+    bscMainnet: "0x7eB627C4CCCd1279a749c15A7F7F30D83FABADC6", //official
 	//For - Avax
     avaxMainnet: "0x", //official
   },
@@ -83,7 +83,7 @@ export default {
 	//For - Tics
     ticsMainnet: "0x16fb89911d7B87daB56510dCB91C2027f9B020b9", //official
 	//For - Bsc
-    bscMainnet: "0x", //official
+    bscMainnet: "0x6BFC02F34d09846dfE2555eAEB61fe7c5dd700F6", //official
 	//For - Avax
     avaxMainnet: "0x", //official
   },
@@ -95,7 +95,7 @@ export default {
 	//For - Tics  
     ticsMainnet: "0x83F77ec45AECd6AaA975D43d7710c5349A078B71", //official
 	//For - Bsc
-    bscMainnet: "0x", //official
+    bscMainnet: "0x16fb89911d7B87daB56510dCB91C2027f9B020b9", //official
 	//For - Avax
     avaxMainnet: "0x", //official
   },
@@ -107,7 +107,7 @@ export default {
 	//For - Tics  
     ticsMainnet: "0xae5095cF0999FeAeF62A07bF0465f4835944D697", //official
 	//For - Bsc
-    bscMainnet: "0x", //official
+    bscMainnet: "0x83F77ec45AECd6AaA975D43d7710c5349A078B71", //official
 	//For - Avax
     avaxMainnet: "0x", //official
   },
@@ -119,7 +119,7 @@ export default {
 	//For - Tics
     ticsMainnet: "0x931765421E5970dda3e3499449A67fb03f4ebD4C", //official
 	//For - Bsc
-    bscMainnet: "0x", //official
+    bscMainnet: "0x931765421E5970dda3e3499449A67fb03f4ebD4C", //official
 	//For - Avax
     avaxMainnet: "0x", //official
   },
@@ -131,7 +131,7 @@ export default {
 	//For - Tics
     ticsMainnet: "0x11A51A136DE68347c9e2e76A2B1F517CE43cfc35", //official
 	//For - Bsc
-    bscMainnet: "0x", //official
+    bscMainnet: "0x11A51A136DE68347c9e2e76A2B1F517CE43cfc35", //official
 	//For - Avax
     avaxMainnet: "0x", //official
   },
